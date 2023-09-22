9a8b577cba0c31ffb5e6a15fe913465797ff245a
Manel <manel.aguilera91@gmail.com>
2023-09-22 16:52:04 +0200

    refactor select user

--- src/components/backoffice/UsersSelector.jsx
+++ src/components/backoffice/UsersSelector.jsx
@@ -29,40 +29,50 @@ const UsersSelector = () => {
     };
   }, []);
 
-
   const setStateImage = (e) => {
     const idUser = parseInt(e.target.value);
     const user = users.filter((user) => user.id === idUser);
 
-    if (user[0].avatar) {
-      setLoadImage(true);
-      setImage(user[0].avatar);
-      setPost({ ...post,
-        [e.target.name] : e.target.value,
-        ['avatar'] : user[0].avatar
-      });
-
-    } else {
+    if (!user[0].avatar) {
       setLoadImage(false);
       setImage('');
-      setPost({ ...post,
-        [e.target.name] : e.target.value,
-        ['avatar'] : ''
-      })
+      setPost({ ...post, [e.target.name]: e.target.value, ["avatar"]: "" });
+      return;
     }
+
+    setImage(user[0].avatar);
+    setLoadImage(true);
+    setPost({
+      ...post,
+      [e.target.name]: e.target.value,
+      ["avatar"]: user[0].avatar,
+    });
+
   };
 
   const handleChange = (e) => {
-    console.log(e.target.value)
+    console.log(e.target.value);
 
     setStateImage(e);
   };
 
   return (
-    <>
+    <div className="md:flex md:items-center md:gap-5">
+      {loadImage ? (
+        <div className="mt-5 flex justify-center">
+          <img
+            name="avatar"
+            id="avatar"
+            className="w-20 h-auto max-w-lg rounded-lg rounded-full"
+            src={`${import.meta.env.VITE_BACKEND_URL}/api/images/${image}`}
+          ></img>
+        </div>
+      ) : (
+        ""
+      )}
       <select
         name="user"
-        className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+        className="w-full md:flex-1 border p-2 mt-2 placeholder-gray-400 rounded-md"
         onChange={handleChange}
       >
         <option>Selecciona un usuario</option>
@@ -76,20 +86,7 @@ const UsersSelector = () => {
           <option>----</option>
         )}
       </select>
-
-      {loadImage ? (
-        <div className="flex justify-center items-center mt-5">
-          <img
-            name="avatar"
-            id="avatar"
-            className="text-center w-full h-auto max-w-lg rounded-lg"
-            src={`${import.meta.env.VITE_BACKEND_URL}/api/images/${image}`}
-          ></img>
-        </div>
-      ) : (
-        ""
-      )}
-    </>
+    </div>
   );
 };
 

714d0ce58bfc7d4f213889a1c92c6b3b3ec75654
Manel <manel.aguilera91@gmail.com>
2023-09-22 10:13:12 +0200

    show and changeImageAvatar when select user

--- src/components/backoffice/UsersSelector.jsx
+++ src/components/backoffice/UsersSelector.jsx
@@ -3,9 +3,12 @@ import { getBearerConfigToken } from "../../Helpers/configs";
 import axios from "axios";
 import useUsers from "../../hooks/useUsers";
 
+
 const UsersSelector = () => {
   
   const { users, setUsers } = useUsers();
+  const [ image, setImage ] = useState('');
+  const [loadImage, setLoadImage] = useState(false);
 
   useEffect(() => {
     const getUser = async () => {
@@ -27,10 +30,26 @@ const UsersSelector = () => {
     };
   }, []);
 
+  const handleChangeImage = async (e) => {
+
+    const idUser = parseInt(e.target.value);
+    const user = users.filter((user) => user.id === idUser);
+    
+    if(user[0].avatar) {
+        setLoadImage(true);
+        setImage(user[0].avatar);
+    }else{
+        setLoadImage(false);
+    }
+
+    };
+
   return (
+    <>
     <select
       id="users"
       className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+      onChange={handleChangeImage}
     >
       {users ? (
         users.map((user, index) => (
@@ -38,10 +57,16 @@ const UsersSelector = () => {
             {user.name}
           </option>
         ))
-      ) : (
+      )
+      : (
         <option>----</option>
       )}
     </select>
+
+    {loadImage ? 
+        <img src={`${import.meta.env.VITE_BACKEND_URL}/api/images/${image}`}></img> 
+    : ''}
+    </>
   );
 };
 
